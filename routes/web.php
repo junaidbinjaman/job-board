8e80edc00561664e473fbe3a5b86d830fb74ccef
junaidbinjaman <me@junaidbinjaman.com>
2025-06-22 10:10:00 +0600

    feat: created new database table for job listing.

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ]
-    ]);
+    return view('jobs', ['jobs' => Job::all()]);
 });
 
 Route::get('/job/{id}', function ($id) {
-    $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-    ];
-
-    $job = \Illuminate\Support\Arr::first($jobs, fn($job) => $job['id'] == $id);
-
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
 
