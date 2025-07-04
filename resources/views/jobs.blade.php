163159ad02026d65e092967825cd8ecd9cb2caac
junaidbinjaman <me@junaidbinjaman.com>
2025-07-04 12:32:32 +0600

    feat: onboarded pagination. I've used sinple pagination here to maintain the website loading performance.

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,7 +2,18 @@
     <x-slot:heading>
         Job Lists
     </x-slot:heading>
-    @foreach($jobs as $job)
-        <li><a href="/job/{{$job['id']}}">{{$job['title']}}</a></li>
-    @endforeach
+    <div class="space-y-4">
+        @foreach($jobs as $job)
+            <a href="/job/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name  }}</div>
+                <div>
+                    <strong>{{$job['title']}}:</strong> Pays {{$job['salary']}} per year
+                </div>
+            </a>
+        @endforeach
+
+        <div>
+            {{ $jobs->links()  }}
+        </div>
+    </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,9 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => Job::all()]);
+    $jobs = Job::with('employer')->cursorPaginate(3);
+
+    return view('jobs', ['jobs' => $jobs]);
 });
 
 Route::get('/job/{id}', function ($id) {
