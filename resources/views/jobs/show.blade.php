f9141d1c3586a11821bbe0eb3243a82917422733
junaidbinjaman <me@junaidbinjaman.com>
2025-07-05 10:10:00 +0600

    feat: the crud operation is completed without authentication. So now, users can read, write, delete and update the jobs.

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -2,6 +2,8 @@
     <x-slot:heading>
         Single job page
     </x-slot:heading>
-    <p><strong>Title:</strong> {{$job['title']}}</p>
-    <p><strong>Salary:</strong> {{$job['salary']}}</p>
+    <p><strong>Title:</strong> {{$job->title}}</p>
+    <p><strong>Salary:</strong> {{$job->salary}}</p>
+
+    <x-button href="/job/{{ $job->id  }}/edit">Edit Jobs</x-button>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,25 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->cursorPaginate(3);
 
     return view('jobs.index', ['jobs' => $jobs]);
 });
 
+// Create
 Route::get('/job/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/job/{id}', function ($id) {
     $job = Job::find($id);
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -35,6 +39,42 @@ Route::post('/jobs', function () {
     ]);
 
     return redirect('/jobs');
+});
+
+// Edit
+Route::get('/job/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/job/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // authorize (on hold..)
+
+    $job = Job::findOrFail($id);
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    // redirect
+    return redirect('/job/'. $job->id);
+
+});
+
+// Destroy
+Route::delete('/job/{id}', function ($id) {
+    // authorize (On hold...)
+
+    Job::findOrFail($id)->delete();
+
+    // redirect
+    return redirect('jobs');
 });
 
 Route::get('/about', function () {
