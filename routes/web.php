1dfe0f375710fb7d9cfd732b9ab93b259489a3f1
junaidbinjaman <me@junaidbinjaman.com>
2025-07-08 07:20:35 +0600

    feat: created a form to sublish new jobs directly from the frontend. - The form design is completed - csrf token is added as well Note: The server-side validation is not yet in effect. So That's something that I have to work on, at this point in time.

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,30 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->cursorPaginate(3);
+    $jobs = Job::with('employer')->latest()->cursorPaginate(3);
 
-    return view('jobs', ['jobs' => $jobs]);
+    return view('jobs.index', ['jobs' => $jobs]);
+});
+
+Route::get('/job/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/job/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    // validation..
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/about', function () {
