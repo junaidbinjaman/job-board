ddf14c87c6b763d447f449896e0b03b80d2f414e
junaidbinjaman <me@junaidbinjaman.com>
2025-07-12 16:53:02 +0600

    feat: refactored the web.php file. So now it contains much less codes compared to before while following all the insdustry best practices

--- routes/web.php
+++ routes/web.php
@@ -2,85 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
+use App\Http\Controllers\JobController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-// Index
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->cursorPaginate(3);
-
-    return view('jobs.index', ['jobs' => $jobs]);
-});
-
-// Create
-Route::get('/job/create', function () {
-    return view('jobs.create');
-});
-
-// Show
-Route::get('/job/{id}', function ($id) {
-    $job = Job::find($id);
-    return view('jobs.show', ['job' => $job]);
-});
-
-// Store
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-// Edit
-Route::get('/job/{id}/edit', function ($id) {
-    $job = Job::find($id);
-    return view('jobs.edit', ['job' => $job]);
-});
-
-// Update
-Route::patch('/job/{id}', function ($id) {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    // authorize (on hold..)
-
-    $job = Job::findOrFail($id);
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary')
-    ]);
-
-    // redirect
-    return redirect('/job/'. $job->id);
-
-});
-
-// Destroy
-Route::delete('/job/{id}', function ($id) {
-    // authorize (On hold...)
-
-    Job::findOrFail($id)->delete();
-
-    // redirect
-    return redirect('jobs');
-});
+Route::resource('jobs', JobController::class);
 
 Route::get('/about', function () {
     return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
 });
